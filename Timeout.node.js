6da55c620e92974e73de921f9cbac9de60d7d5c0
im4LF <him4lf@gmail.com>
2021-03-24 19:17:56 +0300

    Fix Timeout

--- Timeout.node.js
+++ Timeout.node.js
@@ -1,15 +1,16 @@
 'use strict'
 
-module.exports = {
+module.exports = (args, env) => ({
     props: [ 'timeout' ],
     out_: 1,
     _in(message, done) {
         let timeout = typeof this.props.timeout === 'function' ? this.props.timeout(message) : this.props.timeout
-        this.send('out', { t: Date.now() }, done)
-        this.timer = setTimeout(_ => this.send('out', { t: Date.now() }), timeout)
+        this.timer = setTimeout(() => {
+            this.send('out', { t: Date.now() }, done)
+        }, timeout)
     },
     _stop(message, done) {
         if (this.timer) clearTimeout(this.timer)
         done()
     }
-}
+})
